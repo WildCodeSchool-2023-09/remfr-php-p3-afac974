e1357d931e9a4861eacdef277a13a745847ad01a
Narco79 <yannick.ledoux79@gmail.com>
2023-12-19 17:01:21 +0100

    Page A propos sans le css

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,11 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/home', name: 'home_')]
+
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/_a_propos', name: '_a_propos')]
+    public function show(): Response
+    {
+        return $this->render('home/_a_propos.html.twig');
     }
 }
