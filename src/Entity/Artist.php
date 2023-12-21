4c3fec87a5c23996ebaf4f456d4b030e8caef116
PALVAREZ67 <alvarezpierre@hotmail.fr>
2023-12-21 12:11:08 +0100

    correction v2

--- src/Entity/Artist.php
+++ src/Entity/Artist.php
@@ -153,26 +153,6 @@ class Artist implements UserInterface, PasswordAuthenticatedUserInterface
     }
 
 
-    /**
-     * @see UserInterface
-     */
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
-
-        return array_unique($roles);
-    }
-
-    public function setRoles(array $roles): static
-    {
-        $this->roles = $roles;
-
-        return $this;
-    }
-
-
     public function getUser(): ?User
     {
         return $this->user;
