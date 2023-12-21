d1b6a42cad6b92408befc04f978a38dc562814cf
Laura WOLFF <laura.wolff@live.fr>
2023-12-21 09:58:04 +0100

    fix route on homepage, added redirection on logo to home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('')]
+    #[Route('', name:'')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
