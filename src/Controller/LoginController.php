a36ab9ea3dfd17783f44086bc4207a973de249f6
PALVAREZ67 <alvarezpierre@hotmail.fr>
2024-01-04 11:21:43 +0100

    U1.1 page de connexion sans mise en forme v3

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -10,18 +10,6 @@ use Symfony\Component\Security\Core\Security;
 
 class LoginController extends AbstractController
 {
-    public function isConnected(Security $security)
-    {
-        // Vérifier si un utilisateur est connecté
-        if ($security->isGranted('IS_AUTHENTICATED_FULLY')) {
-            // L'utilisateur est connecté
-            // Faire quelque chose ici
-        } else {
-            // L'utilisateur n'est pas connecté
-            // Faire quelque chose d'autre ici
-        }
-        // ...
-    }
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
