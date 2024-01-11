f0eededc33dd866db56e747ff036e034c68d9a1a
antoninbrdl24 <antonin.brondel24@gmail.com>
2024-01-11 06:40:52 +0100

    crud admin artiste et user

---
+++ src/Form/ArtistType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Artist;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArtistType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('description', TextType::class)
+            ->add('photoname', TextType::class)
+            ->add('email', TextType::class)
+           ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Artist::class,
+        ]);
+    }
+}
