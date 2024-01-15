1cfbba271408c912751eab17c74e41818ac71edb
PALVAREZ67 <alvarezpierre@hotmail.fr>
2024-01-15 20:42:31 +0100

    U1.6 a finaliser

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,14 +2,22 @@
 
 namespace App\Controller;
 
+use App\Form\ChangePasswordFormType;
 use App\Entity\User;
 use App\Form\UserType;
+use Symfony\Component\Mime\Address;
 use App\Repository\UserRepository;
+use Symfony\Component\Mime\Email;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/user')]
 class UserController extends AbstractController
@@ -23,14 +31,24 @@ class UserController extends AbstractController
     }
 
     #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
+    public function new(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher,
+        User $user
+    ): Response {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $user->setRoles(['ROLE_USER']);
+            $password = $form->get('password')->getData();
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $password
+            );
+            $user->setPassword($hashedPassword);
             $entityManager->persist($user);
             $entityManager->flush();
 
@@ -52,30 +70,63 @@ class UserController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(UserType::class, $user);
+    public function edit(
+        Request $request,
+        User $user,
+        EntityManagerInterface $entityManager,
+        MailerInterface $mailer,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response {
+        $form = $this->createForm(ChangePasswordFormType::class);
+        // Utilisez le formulaire ChangePasswordFormType
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $user->setRoles(['ROLE_USER']);
-            $entityManager->flush();
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+            $currentPassword = $form->get('plainPassword')->getData();
+            // Utilisez 'plainPassword' pour le champ de mot de passe
+            $newPassword = $form->get('plainPassword')->getData();
+            // Utilisez 'plainPassword' pour le champ de mot de passe
+
+            // Rechargez l'utilisateur depuis la base de données
+            $dbUser = $entityManager->getRepository(User::class)->find($user->getId());
+
+            if ($passwordHasher->isPasswordValid($dbUser, $currentPassword)) {
+                $hashedPassword = $passwordHasher->hashPassword($user, $newPassword);
+                $user->setPassword($hashedPassword);
+                $entityManager->flush();
+            } else {
+                $this->addFlash('passwordError', 'L\'ancien mot de passe ne correspond pas.');
+            }
         }
 
         return $this->render('user/edit.html.twig', [
             'user' => $user,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
     #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
-    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
-    {
+    public function delete(
+        Request $request,
+        User $user,
+        EntityManagerInterface $entityManager,
+        MailerInterface $mailer
+    ): Response {
         if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
             $entityManager->remove($user);
             $entityManager->flush();
+
+            $email = (new Email())
+            ->from($this->getParameter('mailer_from'))
+            ->to($user->getEmail())
+            ->subject('Votre compte AFAC 974 à bien été supprimé !')
+            ->html($this->renderView('user/emailDel.html.twig'));
+
+            $mailer->send($email);
+
+            return $this->redirectToRoute('app_user_show', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -11,21 +11,31 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('password', PasswordType::class)
-            ->add('name', TextType::class)
-            ->add('lastname', TextType::class)
-            ->add('artist_id', EntityType::class, [
-                'class' => Artist::class,
-        'choice_label' => 'id',
-            ])
-        ;
+        ->add('email', EmailType::class)
+        ->add('currentPassword', PasswordType::class, [
+            'label' => 'Ancien mot de passe',
+            'mapped' => false, // Ce champ n'est pas mappé à l'entité User.
+        ])
+        ->add('password', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'invalid_message' => 'Les champs de mot de passe doivent correspondre.',
+            'required' => true,
+            'first_options'  => ['label' => 'Nouveau mot de passe'],
+            'second_options' => ['label' => 'Répéter le nouveau mot de passe'],
+        ])
+        ->add('name', TextType::class)
+        ->add('lastname', TextType::class)
+        ->add('artist_id', EntityType::class, [
+            'class' => Artist::class,
+            'choice_label' => 'id',
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
