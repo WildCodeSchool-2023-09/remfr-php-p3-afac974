7a26aa4280f99968dcd41a7f89d6eb7c98ac7e01
LauraW-code <laura.wolff@live.fr>
2024-01-24 17:32:49 +0100

    fix typeFixtures

--- src/DataFixtures/ArtworkFixtures.php
+++ src/DataFixtures/ArtworkFixtures.php
@@ -10,42 +10,42 @@ use App\Entity\Artwork;
 class ArtworkFixtures extends Fixture implements DependentFixtureInterface
 {
     public const ARTWORK = [
-        ['is_signed' => false, 'is_unique' => true, 'title' => 'L\'ours',
+        ['is_signed' => false, 'is_unique' => true, 'title' => 'L\'ours', 'type' => 'type_Sculpture',
         'description' => 'Lorem ipsum dolor sit amet. Et dicta placeat aut deserunt vero At enim ullam ab repellat 
          nihil et enim possimus est voluptatem officia vel delectus excepturi. Id sunt doloremque aut alias expedita
          et expedita consequatur. Id voluptatibus temporibus id saepe nobis et nobis deserunt. Aut quas eveniet ut 
          sapiente fugiat eum doloremque dolorem rem pariatur iste qui omnis earum.', 'year' => 1999,
          'height' => 110, 'width' => 65, 'artist' => 'Théo Payet', 'picture' => 'bear.png'],
 
-        ['is_signed' => true, 'is_unique' => true, 'title' => 'L\'homme',
+        ['is_signed' => true, 'is_unique' => true, 'title' => 'L\'homme', 'type' => 'type_Peinture',
         'description' => 'Lorem ipsum dolor sit amet. Et dicta placeat aut deserunt vero At enim ullam ab repellat 
         nihil et enim possimus est voluptatem officia vel delectus excepturi. Id sunt doloremque aut alias expedita 
         et expedita consequatur. Id voluptatibus temporibus id saepe nobis et nobis deserunt. Aut quas eveniet ut 
         sapiente fugiat eum doloremque dolorem rem pariatur iste qui omnis earum.', 'year' => 2002,
         'height' => 110, 'width' => 65, 'artist' => 'Théo Payet', 'picture' => 'portrait.png'],
 
-        ['is_signed' => true, 'is_unique' => true, 'title' => 'L\'oiseau imaginé',
+        ['is_signed' => true, 'is_unique' => true, 'title' => 'L\'oiseau imaginé', 'type' => 'type_Peinture',
         'description' => 'Lorem ipsum dolor sit amet. Et dicta placeat aut deserunt vero At enim ullam ab repellat 
         nihil et enim possimus est voluptatem officia vel delectus excepturi. Id sunt doloremque aut alias expedita 
         et expedita consequatur. Id voluptatibus temporibus id saepe nobis et nobis deserunt. Aut quas eveniet ut 
         sapiente fugiat eum doloremque dolorem rem pariatur iste qui omnis earum.', 'year' => 2022,
         'height' => 65, 'width' => 65, 'artist' => 'Hanaé Grondin', 'picture' => 'bird.png'],
 
-        ['is_signed' => false, 'is_unique' => false, 'title' => 'Forêt brumeuse',
+        ['is_signed' => false, 'is_unique' => false, 'title' => 'Forêt brumeuse', 'type' => 'type_Photographie',
         'description' => 'Lorem ipsum dolor sit amet. Et dicta placeat aut deserunt vero At enim ullam ab repellat 
         nihil et enim possimus est voluptatem officia vel delectus excepturi. Id sunt doloremque aut alias expedita 
         et expedita consequatur. Id voluptatibus temporibus id saepe nobis et nobis deserunt. Aut quas eveniet ut 
         sapiente fugiat eum doloremque dolorem rem pariatur iste qui omnis earum.', 'year' => 2018,
         'height' => 45, 'width' => 45, 'artist' => 'Maya Riviere', 'picture' => 'forest.png'],
 
-        ['is_signed' => true, 'is_unique' => true, 'title' => 'Les lunettes d\'Andy',
+        ['is_signed' => true, 'is_unique' => true, 'title' => 'Les lunettes d\'Andy', 'type' => 'type_Peinture',
         'description' => 'Lorem ipsum dolor sit amet. Et dicta placeat aut deserunt vero At enim ullam ab repellat 
         nihil et enim possimus est voluptatem officia vel delectus excepturi. Id sunt doloremque aut alias expedita 
         et expedita consequatur. Id voluptatibus temporibus id saepe nobis et nobis deserunt. Aut quas eveniet ut 
         sapiente fugiat eum doloremque dolorem rem pariatur iste qui omnis earum.', 'year' => 2005,
         'height' => 110, 'width' => 65, 'artist' => 'Raphaël Damour', 'picture' => 'glasses.png'],
 
-        ['is_signed' => true, 'is_unique' => true, 'title' => 'Paysage Onirique',
+        ['is_signed' => true, 'is_unique' => true, 'title' => 'Paysage Onirique', 'type' => 'type_Peinture',
         'description' => 'Lorem ipsum dolor sit amet. Et dicta placeat aut deserunt vero At enim ullam ab repellat 
         nihil et enim possimus est voluptatem officia vel delectus excepturi. Id sunt doloremque aut alias expedita 
         et expedita consequatur. Id voluptatibus temporibus id saepe nobis et nobis deserunt. Aut quas eveniet ut 
@@ -75,6 +75,7 @@ class ArtworkFixtures extends Fixture implements DependentFixtureInterface
             $artwork->setYear($artworkName['year']);
             $artwork->setHeight($artworkName['height']);
             $artwork->setWidth($artworkName['width']);
+            $artwork->setType($this->getReference($artworkName['type']));
             $artwork->setArtist($this->getReference('artist_doe'));
             $artwork->setPicture($artworkName['picture']);
 
@@ -88,6 +89,7 @@ class ArtworkFixtures extends Fixture implements DependentFixtureInterface
         // Tu retournes ici toutes les classes de fixtures dont ProgramFixtures dépend
         return [
           ArtistFixtures::class,
+          TypeFixtures::class,
         ];
     }
 }
